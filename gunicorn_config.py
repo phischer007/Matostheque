85c0a211931fd58f12b49c6106e1c81b4edae64a
phischer007 <97169840+phischer007@users.noreply.github.com>
2025-11-24 14:50:22 +0100

    Update Gunicorn bind address for server

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,5 @@
 import multiprocessing
 
-# bind = "127.0.0.1:8000" # django server port, localhost
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2
